08f933c9c2850c5c41cb64797e80310c2df35f2a
matrix-bro <razmhz11@gmail.com>
2024-01-16 21:19:44 +0545

    Designing Home page, blog list and blog detail page

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('posts/', views.posts, name='posts'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
 
--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from app.models.blog import Blog
 
 def index(request):
-    return HttpResponse("<h1>hello django</h1>")
+    # Today's top 2 posts
+    # TODO: Different query
+    today_two_posts = Blog.published.all()[:2]
+
+    # Today's main post
+    # TODO: Different query
+    today_main_post = Blog.published.first()
+    
+    # Recent Five Posts
+    # TODO: Different query
+    posts = Blog.published.all()[:5]
+
+    print(today_main_post)
+
+    return render(request, 'app/index.html', {
+        "today": today_two_posts,
+        "today_main": today_main_post,
+        "posts": posts,
+    })
 
 def posts(request):
     posts = Blog.published.all()
