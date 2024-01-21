8661b1e16d29b717a84ee4007cf0699ac02bb192
matrix-bro <razmhz11@gmail.com>
2024-01-21 18:13:46 +0545

    feat:Created custom tag for displaying most commented posts

--- app/templatetags/blog_tags.py
+++ app/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from app.models.blog import Blog
+from django.db.models import Count
 
 register = template.Library()
 
@@ -11,3 +12,7 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Blog.published.order_by('-created_at')[:count]
     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Blog.published.annotate(total_comments=Count('comments')).filter(total_comments__gte=1).order_by('-total_comments')[:count]
